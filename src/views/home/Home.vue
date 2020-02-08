251588cfb4b89f87fee375f6eaff43bd61bb04e3
hzebin <1763152010@qq.com>
2020-02-08 14:31:03 +0800

    create home navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-    <h1>首页</h1>
+    <NavBar class="home-nav">
+      <div slot="left"></div>
+      <div slot="center">购物街</div>
+      <div slot="right"></div>
+    </NavBar>
   </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar';
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: var(--color-background);
+    box-shadow: 0px 2px 1px rgba(100, 100, 100, .1);
+  }
 </style>
